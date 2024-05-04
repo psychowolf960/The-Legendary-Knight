3fced407b92a45c2e5f82676238075fee21a8626
psychowolf960 <119869367+psychowolf960@users.noreply.github.com>
2024-05-04 09:04:24 +0200

    Fixes

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -9,7 +9,7 @@ class World extends Control:
 		self.x = 0
 
 @onready var world_1 = $MarginContainer2/HBoxContainer/World1
-@onready var world_4 = $MarginContainer2/HBoxContainer/World4
+
 @onready var world_2 = $MarginContainer2/HBoxContainer/World2
 @onready var world_3 = $MarginContainer2/HBoxContainer/World3
 
@@ -23,48 +23,32 @@ func animate_sliding(direction: String):
 
 @onready var worlds = []
 var world_indices = {}
-
 var animation_in_progress = false
 
 func _ready():
-	var world_1_obj = World.new("World 1")
-	var world_2_obj = World.new("World 2")
-	var world_3_obj = World.new("World 3")
-	var world_4_obj = World.new("World 4")
-
-	world_1.add_child(world_1_obj)
-	world_2.add_child(world_2_obj)
-	world_3.add_child(world_3_obj)
-	world_4.add_child(world_4_obj)
-
-	worlds.append(world_1_obj)
-	worlds.append(world_2_obj)
-	worlds.append(world_3_obj)
-	worlds.append(world_4_obj)
-
-	world_indices[world_1_obj] = 1
-	world_indices[world_2_obj] = 2
-	world_indices[world_3_obj] = 3
-	world_indices[world_4_obj] = 4
+	worlds = [world_1, world_2, world_3]
+	for i in range(worlds.size()):
+		world_indices[worlds[i]] = i + 1
 
 func _start_animation_right():
 	animation_in_progress = true
 	var hbox = $MarginContainer2/HBoxContainer
-	var temp_worlds = worlds.duplicate()
-	for i in range(temp_worlds.size()):
-		if world_indices[temp_worlds[i]] == 4:
-			world_indices[temp_worlds[i]] = 1
+	for world in worlds:
+		if world_indices[world] == 4:
+			world_indices[world] = 1
 		else:
-			world_indices[temp_worlds[i]] += 1
-	for i in range(temp_worlds.size()):
-		if world_indices[temp_worlds[i]] == 1:
-			hbox.move_child(temp_worlds[i].get_parent(), 0)
-		elif world_indices[temp_worlds[i]] == 2:
-			hbox.move_child(temp_worlds[i].get_parent(), 1)
-		elif world_indices[temp_worlds[i]] == 3:
-			hbox.move_child(temp_worlds[i].get_parent(), 2)
-		elif world_indices[temp_worlds[i]] == 4:
-			hbox.move_child(temp_worlds[i].get_parent(), 3)
+			world_indices[world] += 1
+	for i in range(worlds.size()):
+		hbox.move_child(worlds[i], world_indices[worlds[i]] - 1)
+	
+	# Print tests to check if the nodes have correctly moved inside the HBoxContainer
+	print("After animation, the order of worlds in this damn HBoxContainer is:")
+	for i in range(hbox.get_child_count()):
+		print(hbox.get_child(i).name)
+	
+	for world in worlds:
+		print("Mfckin World ", world.name, " is at index ", world_indices[world])
+	
 	animation_in_progress = false
 
 func _on_play_pressed():
@@ -79,7 +63,7 @@ func _on_exit_pressed():
 func _input(event: InputEvent):
 	if event is InputEventKey:
 		if Input.is_action_just_pressed("ui_right") and not animation_in_progress:
-			print("gay")
+			print("ok")
 			animate_sliding("right")
 		elif Input.is_action_just_pressed("ui_left") and not animation_in_progress:
 			print(world_indices)
